c6813a3aa39292bf8a16c191e7c42ee11472fe87
Alvaro Redondo <redosan1@gmail.com>
2018-04-15 19:16:33 +0200

    Initial commit

---
+++ app/src/main/java/website/timrobinson/opencvtutorial/TriadaActivity.java
@@ -0,0 +1,14 @@
+package website.timrobinson.opencvtutorial;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TriadaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_triada);
+    }
+
+}
